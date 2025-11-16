52415a33309b83648fd6371824c0e16f837ab8e1
RazorBackRoar <RazorBackRoar@users.noreply.github.com>
2025-11-16 04:08:14 -0700

    Fixes

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ NC='\033[0m'
 
 # App configuration
 APP_NAME="4Charm"
-APP_VERSION="3.0.1110"
+APP_VERSION="3.0.1112"
 PYTHON_EXE="$HOME/.venvs/razor/bin/python"
 DIST_DIR="dist"
 APP_PATH="$DIST_DIR/${APP_NAME}.app"
