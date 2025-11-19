08b43f49d2371044683f264f7e880a3e74176b04
RazorBackRoar <RazorBackRoar@users.noreply.github.com>
2025-11-19 07:12:51 -0700

    Fixes

--- main.py
+++ main.py
@@ -1237,9 +1237,6 @@ class MainWindow(QMainWindow):
 """
         )
         url_frame_layout.addWidget(self.url_input)
-        url_frame_layout.setAlignment(
-            self.url_input, Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft
-        )
 
         url_layout.addWidget(url_frame)
 
@@ -1374,7 +1371,7 @@ class MainWindow(QMainWindow):
         self.status_bar.showMessage("Ready")
 
         # Add version label to bottom right of status bar
-        version_label = QLabel("v5.2.0")
+        version_label = QLabel("v5.4.0")
         version_label.setStyleSheet("font-size: 11px; color: #666; padding: 0 8px;")
         self.status_bar.addPermanentWidget(version_label)
 
