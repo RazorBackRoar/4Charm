76a5639f4a5f15a9e51effe84a1d020273fab84d
RazorBackRoar <184320956+RazorBackRoar@users.noreply.github.com>
2026-01-24 03:34:47 -0700

    chore: bump version to 6.4.17

--- src/four_charm/__init__.py
+++ src/four_charm/__init__.py
@@ -5,4 +5,4 @@ from importlib.metadata import version, PackageNotFoundError
 try:
     __version__ = version("4Charm")
 except PackageNotFoundError:
-    __version__ = "6.4.16"  # Fallback for bundled apps
+    __version__ = "6.4.17"  # Fallback for bundled apps
